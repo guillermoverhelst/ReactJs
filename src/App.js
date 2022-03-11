018f68e70615187575225be5750d5ef914c5bda4
guillermo <guillermoverhelst26@gmail.com>
2022-03-10 22:06:26 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tasks from './componentes/Tasks';
+import tasks from './Jsons/tasks.json';
+import TaskForm from './componentes/TaskForm';
+import Post from './componentes/Post';
+import {BrowserRouter as Router ,Link,Route,Routes} from 'react-router-dom';
+class App extends React.Component{
+  
+  state={
+    tasks:tasks
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  addTask = (title,description) => {
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length    
+    }
+    console.log(newTask)
+    this.setState({
+      tasks:[...this.state.tasks,newTask]
+    })
+  }
+
+  deleteTask= (id) =>{
+    const newTasks = this.state.tasks.filter(task => task.id !== id)
+    this.setState({
+      tasks: newTasks
+    })
+  }
+
+  checkDone = id =>{
+    const newTask = this.state.tasks.map(task =>{
+      if(task.id === id){
+        task.done = !task.done
+      }
+      return task;
+    });
+    this.setState({
+      tasks: newTask
+    })
+  
+  }
+
+  render(){
+    return (
+      <div>
+       <Router>
+         <Link to="/">Home</Link>
+         <br/>
+         <Link to="/posts">Posts</Link>
+         <Routes>
+          <Route path="/posts" element={<Post/>}/>
+          <Route path="/" element={<div><TaskForm addTask={this.addTask}/><Tasks tasks={this.state.tasks} 
+              deleteTask={this.deleteTask} 
+              checkDone={this.checkDone}
+            /></div>}></Route>
+         </Routes>
+       </Router>                  
+      </div>
+    )
+  }
 }
 
 export default App;
